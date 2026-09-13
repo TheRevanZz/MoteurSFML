20c7cb807707f4cd4fb0bd4c96014e4a43ff888e
Sylvio PELAGE--MAXIME <sylvio0801@gmail.com>
2026-09-13 16:13:13 +0200

    Changement setup.sh$

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-﻿#!/usr/bin/bash
+﻿#!/usr/bin/sh
 
-conan install . -s compiler=gcc -s compiler.version=16.1 -s compiler.libcxx=libstdc++11 --build missing -s compiler.cppstd=20 --output-folder out\build\conan-release
-conan install . -s compiler=gcc -s compiler.version=16.1 -s compiler.libcxx=libstdc++11 --build missing -s compiler.cppstd=20 --output-folder out\build\conan-debug -s build_type=Debug
+conan install . -s compiler=gcc -s compiler.version=16.1 -s compiler.libcxx=libstdc++11 --build missing -s compiler.cppstd=20 --output-folder out/build/conan-release
+conan install . -s compiler=gcc -s compiler.version=16.1 -s compiler.libcxx=libstdc++11 --build missing -s compiler.cppstd=20 --output-folder out/buildM/conan-debug -s build_type=Debug
 
-mkdir build -ErrorAction SilentlyContinue
+mkdir build
 
 cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE="out/build/conan-debug/conan_toolchain.cmake" -G "Unix Makefiles" -DCMAKE_CXX_COMPILER="g++" -DCMAKE_C_COMPILER="gcc" -DCMAKE_BUILD_TYPE="Debug"
 cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE="out/build/conan-release/conan_toolchain.cmake" -G "Unix Makefiles" -DCMAKE_CXX_COMPILER="g++" -DCMAKE_C_COMPILER="gcc" -DCMAKE_BUILD_TYPE="Release"
