82fdd7276e490aa9339ace020607665ce7d1b86d
Sylvio PELAGE--MAXIME <sylvio0801@gmail.com>
2026-09-17 13:53:12 +0200

    CHangement structure projet, debut implementation ennemie

--- src/Class/Character/Player/Player.cpp
+++ src/Class/Character/Player/Player.cpp
@@ -2,53 +2,63 @@
 // Created by sylvi on 12/09/2026.
 //
 
-#include "Player.h"
-#include "Time/Time.h"
 #include <fstream>
 #include <iostream>
 #include <ryml.hpp>
 #include <ryml_std.hpp>
 
+#include "Player.h"
 #include "Debug.h"
-#include "Utils/Utils.h"
+#include "Game/Utils/Utils.h"
+#include "Game/WindowData/WindowData.h"
+#include "Game/Time/Time.h"
 
 
-Player::Player(const sf::Texture &texture, const sf::Vector2u &screenSize)
-    : BaseCharacter(texture, 150), _screenSize(screenSize) {
+Player::Player(const sf::Texture& texture)
+    : BaseCharacter(texture, 150), _screenSize(WindowData::getScreenSize())
+{
     _id = _count;
     _sprite.setScale({.15f, .15f});
-    _bounds = _sprite.getGlobalBounds();
 
-    if (_id == 0) {
+    if (_id == 0)
+    {
         _keymapPath = "ressources/config/keymap1.yml";
-    } else {
+    }
+    else
+    {
         _keymapPath = "ressources/config/keymap2.yml";
     }
     assert(LoadKeymap(_keymapPath) == true && ("ERREUR DANS LE CHARGEMENT " + _keymapPath).c_str());
     // keymap.at("Left").second = sf::Keyboard::Key::Left;
+    
+     _sprite.setOrigin({_sprite.getLocalBounds().size.x / 2.f, _sprite.getLocalBounds().size.y / 2.f});
     _count++;
-    _sprite.setOrigin({_bounds.size.x / 2.f, _bounds.size.y / 2.f});
 }
 
-void Player::setPosition(const WorldPoint &newPosition) {
+void Player::setPosition(const WorldPoint& newPosition)
+{
     _position = newPosition;
     _sprite.setPosition(sf::Vector2f(GameWindow::toScreenPoint(newPosition, _screenSize)));
 }
 
-sf::Vector2f Player::getScaledSize() const {
-    const auto &scale = this->_sprite.getScale();
-    const auto &size = this->_sprite.getLocalBounds().size;
+sf::Vector2f Player::getScaledSize() const
+{
+    const auto& scale = this->_sprite.getScale();
+    const auto& size = this->_sprite.getLocalBounds().size;
 
     return {scale.x * size.x, scale.y * size.y};
 }
 
 const sf::Transform Player::getTransform() const
-{ return this->_sprite.getTransform(); }
+{
+    return this->_sprite.getTransform();
+}
 
-void Player::move(const sf::Vector2f &offset) {
+void Player::move(const sf::Vector2f& offset)
+{
     //Si offset.length = offset.x c'est équivalent à ce que offset.y soit egal a 0.
     // offset.length c'est Vx**2 + y**2 donc si Vx**2 + y**2 = 0 alors c'est que y = 0 car Vx**2 + 0 = x
-    _sprite.move({ offset.x, -offset.y});
+    _sprite.move({offset.x, -offset.y});
 
     //si à droite de l'écran
     if (_sprite.getPosition().x > _screenSize.x - getScaledSize().x / 2)
@@ -85,7 +95,7 @@ void Player::progressiveRotate(float targetAngle)
 {
     const auto& dt = Time::deltaTime();
 
-    float currentAngle = _sprite.getRotation().asDegrees();
+    const float currentAngle = _sprite.getRotation().asDegrees();
     // pr atteindre le target angle je dois rotate de angle°
     float angle = targetAngle - currentAngle;
 
@@ -99,7 +109,7 @@ void Player::progressiveRotate(float targetAngle)
         angle += 360.f;
     }
 
-    float tempRotation = _rotateSpeed * dt;
+    float tempRotation;
     if (angle > 0.f)
     {
         if (_rotateSpeed * dt < angle)
@@ -118,46 +128,49 @@ void Player::progressiveRotate(float targetAngle)
 
         rotate(-tempRotation);
     }
-
-    // std::cout << angle << " : " << _rotateSpeed * dt << std::endl;
 }
 
-void Player::update() {
+void Player::update()
+{
     handleMovement();
-    // window.draw(this->_sprite);
     handleRotation();
-    // std::cout << _targetRotation << std::endl;
     progressiveRotate(_targetRotation);
 }
 
-void Player::handleMovement() {
-    const auto &dt = Time::deltaTime();
+void Player::handleMovement()
+{
+    const auto& dt = Time::deltaTime();
     auto offset = sf::Vector2f(0, 0);
     if (
         PlayerIsDoingAction(EActionTag::LEFT)
-    ) {
+    )
+    {
         offset.x -= 1;
     }
-    if (PlayerIsDoingAction(EActionTag::RIGHT)) {
+    if (PlayerIsDoingAction(EActionTag::RIGHT))
+    {
         offset.x += 1;
     }
-    if (PlayerIsDoingAction(EActionTag::UP)) {
+    if (PlayerIsDoingAction(EActionTag::UP))
+    {
         // Up key pressed.
         offset.y += 1;
     }
-    if (PlayerIsDoingAction(EActionTag::DOWN)) {
+    if (PlayerIsDoingAction(EActionTag::DOWN))
+    {
         // Down key pressed.
         offset.y -= 1;
     }
 
     DEBUG_ONLY(
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::K)) {
-            std::cout << applyBonusToStat(_speed, EBonusCategory::SPEED) << "\n";
-            std::cout << _speed << "\n";
+        std::cout << applyBonusToStat(_speed, EBonusCategory::SPEED) << "\n";
+        std::cout << _speed << "\n";
         }
     )
 
-    if (offset.length() > 0) {
+    if (offset.length() > 0)
+    {
         //Le vecteur est normalisé comme ça la valeur de length est toujours égal a 1, donc les déplacements sont toujours de même vitesse
         // même en diagonale
         offset = offset.normalized();
@@ -166,7 +179,8 @@ void Player::handleMovement() {
 }
 
 
-bool Player::PlayerIsDoingAction(const EActionTag action_tag) const {
+bool Player::PlayerIsDoingAction(const EActionTag action_tag) const
+{
     const auto KeymapValue = keymap.get(action_tag);
     if (!KeymapValue.has_value())
         return false;
@@ -175,29 +189,32 @@ bool Player::PlayerIsDoingAction(const EActionTag action_tag) const {
     return sf::Keyboard::isKeyPressed(KeymapValue.value().second.value());
 }
 
-bool Player::LoadKeymap(const std::string &keymapPath) {
-    const auto &content = Utils::getFileContent(keymapPath);
+bool Player::LoadKeymap(const std::string& keymapPath)
+{
+    const auto& content = Utils::getFileContent(keymapPath);
     if (!content.has_value())
         return false;
 
     ryml::Tree YamlContent = ryml::parse_in_arena(c4::to_csubstr(content.value()));
-    
+
     if (!YamlContent.rootref().is_map())
     {
         abort();
     }
     const auto touches = YamlContent["touches"];
 
-    for (const std::string_view &key: GetEActionTagsValues()) {
+    for (const std::string_view& key : GetEActionTagsValues())
+    {
         const auto keyStr = std::string(key);
-        const auto &keyTag = StringToEActionTag(keyStr);
+        const auto& keyTag = StringToEActionTag(keyStr);
         if (!keyTag.has_value())
             continue;
 
         //Le code de la touche ( ex: 25 -> Z)
         const ryml::csubstr KeyCodeCSubStr = touches[keyStr.c_str()].val();
 
-        try {
+        try
+        {
             int KeyCode = std::stoi(Utils::csubtrToString(KeyCodeCSubStr));
 
             auto OptionalKeymapValue = keymap.get(keyTag.value());
@@ -208,7 +225,9 @@ bool Player::LoadKeymap(const std::string &keymapPath) {
             KeymapValue.second = static_cast<sf::Keyboard::Key>(KeyCode);
 
             keymap.update(keyTag.value(), KeymapValue);
-        } catch (...) {
+        }
+        catch (...)
+        {
             DEBUG_ONLY(
                 std::cout << "Une erreur à lieu lors du chargement pour la clé" << keyStr << "\n";
             )
@@ -221,7 +240,8 @@ bool Player::LoadKeymap(const std::string &keymapPath) {
     return true;
 }
 
-bool Player::ChangeKey(const EActionTag &action_tag, const sf::Keyboard::Key &new_key) {
+bool Player::ChangeKey(const EActionTag& action_tag, const sf::Keyboard::Key& new_key)
+{
     auto OptionalValue = keymap.get(action_tag);
     if (!OptionalValue.has_value())
         return false;
@@ -232,34 +252,34 @@ bool Player::ChangeKey(const EActionTag &action_tag, const sf::Keyboard::Key &ne
 
     keymap.update(action_tag, {keyDisplayName, KeyValue});
 
-    const auto &content = Utils::getFileContent(_keymapPath);
+    const auto& content = Utils::getFileContent(_keymapPath);
     if (!content.has_value())
         return false;
 
     ryml::Tree YamlContent = ryml::parse_in_arena(
         ryml::to_csubstr(content.value())
     );
-    
+
     if (!YamlContent.rootref().is_map())
     {
         abort();
     }
-    
-    const auto &action_tag_str = EActionTagToString(action_tag);
+
+    const auto& action_tag_str = EActionTagToString(action_tag);
 
     if (!action_tag_str.has_value())
         return false;
 
     YamlContent["touches"][action_tag_str.value().c_str()] << static_cast<int>(new_key);
 
-    FILE *yamlFile = fopen(_keymapPath.c_str(), "w");
+    FILE* yamlFile = fopen(_keymapPath.c_str(), "w");
 
     if (yamlFile == nullptr)
         return false;
 
     ryml::emit_yaml(YamlContent, yamlFile);
     std::cout << "[KEYMAP] Keymap " << _keymapPath << " updated\n";
-    if (int returnCode =  fclose(yamlFile); returnCode == EOF)
+    if (int returnCode = fclose(yamlFile); returnCode == EOF)
     {
         DEBUG_ONLY(
             std::cerr << "ERREUR DANS LA FERMETURE DU FICHIER\n"
@@ -269,13 +289,16 @@ bool Player::ChangeKey(const EActionTag &action_tag, const sf::Keyboard::Key &ne
     return true;
 }
 
-void Player::Collision(const std::shared_ptr<IGameComponent> &otherComponent) const {
+void Player::Collision(const std::shared_ptr<IGameComponent>& otherComponent) const
+{
     IGameComponent::Collision(otherComponent);
-    abort();
+    // abort();
 }
 
-float Player::applyBonusToStat(const float &stat, const EBonusCategory bonusCategory) const {
-    auto _targetBonuses = _bonuses | std::views::filter([bonusCategory](const auto& bonus) {
+float Player::applyBonusToStat(const float& stat, const EBonusCategory bonusCategory) const
+{
+    auto _targetBonuses = _bonuses | std::views::filter([bonusCategory](const auto& bonus)
+    {
         return bonus->getBonusCategory() == bonusCategory;
     });
 
@@ -285,7 +308,8 @@ float Player::applyBonusToStat(const float &stat, const EBonusCategory bonusCate
     float final_stat = (*_targetBonuses.begin())->getApplyedBonus(stat);
     auto it = _targetBonuses.begin();
     ++it;
-    for (; it != _targetBonuses.end(); ++it) {
+    for (; it != _targetBonuses.end(); ++it)
+    {
         final_stat = (*it)->getApplyedBonus(final_stat);
     }
     return final_stat;
@@ -293,7 +317,6 @@ float Player::applyBonusToStat(const float &stat, const EBonusCategory bonusCate
 
 void Player::handleRotation()
 {
-
     if (PlayerIsDoingAction(EActionTag::LEFT))
     {
         _targetRotation = 180;
