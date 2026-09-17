37d32709b3309b4e9255bd8174a4145b9d4715f7
6666SD9999 <selimdjaafri00@gmail.com>
2026-09-17 15:18:11 +0200

    feature acceleration v6

--- src/Class/Player/Player.h
+++ src/Class/Player/Player.h
@@ -13,41 +13,43 @@
 
 class Player : public IUpdateable, public IDrawable {
 
-    public:
-        explicit Player(const sf::Texture& texture, const sf::Vector2u& screenSize);
+public:
+    explicit Player(const sf::Texture& texture, const sf::Vector2u& screenSize);
 
-        const sf::Sprite& getSprite() const { return this->_sprite; }
+    const sf::Sprite& getSprite() const { return this->_sprite; }
 
-        void handleEvent(const std::optional<sf::Event> &event);
+    void handleEvent(const std::optional<sf::Event>& event);
 
-        void setPosition(const WorldPoint& newPosition);
-        
-        sf::Vector2f getSize() const { return this->_sprite.getLocalBounds().size; }
-        sf::Vector2f getScaledSize() const;
-        sf::Vector2f getPosition() const { return this->_sprite.getPosition(); }
-    
-        void move(const sf::Vector2f& offset);
+    void setPosition(const WorldPoint& newPosition);
 
-        explicit operator sf::Sprite() const { return this->_sprite; }
-    
-        void update(sf::RenderWindow& window) override;
-        const sf::Drawable* getDrawable() const override { return &this->_sprite;}
-        
+    sf::Vector2f getSize() const { return this->_sprite.getLocalBounds().size; }
+    sf::Vector2f getScaledSize() const;
+    sf::Vector2f getPosition() const { return this->_sprite.getPosition(); }
 
-    protected:
-        sf::Sprite _sprite;
-        WorldPoint _position;
-        sf::Vector2u _screenSize;
-    
-        float _velocityX = 0.f;  
-        float _velocityY = 0.f;  
-        float _mass = 1.f;       
-        float _maxSpeed = 300.f; 
-        float _friction = 0.95f; 
-        float _thrustForce = 300.f;
-    
-    private:
-        void handleMovement();
+    void move(const sf::Vector2f& offset);
+
+    explicit operator sf::Sprite() const { return this->_sprite; }
+
+    void update(sf::RenderWindow& window) override;
+
+    const sf::Drawable& getDrawable() const override {
+        return this->_sprite;
+    }
+
+protected:
+    sf::Sprite _sprite;
+    WorldPoint _position;
+    sf::Vector2u _screenSize;
+
+    float _velocityX = 0.f;
+    float _velocityY = 0.f;
+    float _mass = 1.f;
+    float _maxSpeed = 300.f;
+    float _friction = 0.95f;
+    float _thrustForce = 300.f;
+
+private:
+    void handleMovement();
 };
 
 
