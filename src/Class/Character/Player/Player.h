d710eacbf77108bab1b1f9df26c287bb7e96d2a6
6666SD9999 <selimdjaafri00@gmail.com>
2026-09-21 10:38:39 +0200

    v1

--- src/Class/Character/Player/Player.h
+++ src/Class/Character/Player/Player.h
@@ -86,9 +86,9 @@ protected:
     float _velocityX = 0.f;
     float _velocityY = 0.f;
     float _mass = 1.f;
-    float _maxSpeed = 300.f;
-    float _friction = 0.95f;
-    float _thrustForce = 300.f;
+    float _maxSpeed = 150.f;
+    float _friction = 0.90f;
+    float _thrustForce = 500.f;
     
     int _id = 0;
     std::string _keymapPath = "ressources/config/keymap1.yml";
